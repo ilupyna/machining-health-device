4fb06eed1f956c6ea3570ff522884a71541064ec
SexyPumpkin <62780669+SexyPumpkin@users.noreply.github.com>
2022-01-26 18:00:01 +0200

    Third version via upload

--- machining-health-device.ino
+++ machining-health-device.ino
@@ -1,254 +1,163 @@
-#include <ESP8266WiFi.h>
-#include <ESP8266WebServer.h>
 #include <LittleFS.h>
-
-//#include <ESP8266mDNS.h>
-
-#include <Adafruit_BMP085_U.h>
-#include <Adafruit_HMC5883_U.h>
 #include "I2Cdev.h"
 #include "MPU6050.h"
-//#include <Vector.h>
 
 #include <time.h>
 #include <String>
 
 #define LED_BUILTIN 2
-#define MAX_DATASIZE 1500
+#define MAX_DATA_SIZE 1000
 
-
-const char* ssid = "yourNetwork";
-const char* password = "secretPassword";
-
-String last_console_text = "";
 bool led_state = true;  //set off by default
-
 bool SensorLive = false;
 
 
-struct dataItem {
-  time_t timeStomp;
-  int16_t ax, ay, az;
-  int16_t gx, gy, gz;
-};
+unsigned long Start, Finish, s2, f2, savetime = 0;
+size_t iterations;
 
-//Vector<dataItem> dataStorage;
-dataItem dataStorage[MAX_DATASIZE];
-size_t storageSize = 0;
-
-ESP8266WebServer server(80);
 
 MPU6050 accelgyro;
 int16_t ax, ay, az;
 int16_t gx, gy, gz;
-Adafruit_BMP085_Unified bmp = Adafruit_BMP085_Unified(10085);
-Adafruit_HMC5883_Unified mag = Adafruit_HMC5883_Unified(12345);
 
 
-const String preparedHtmlPage = 
-  "<!DOCTYPE html>"\
-    "<html>"\
-      "<head>"\
-        "<title>ESP8266 Demo</title>"\
-        "<link rel=\"icon\" href=\"data:,\">"\
-        "<style>"\
-          "body {background-color:#2c2c34; font-family:Arial, Helvetica, Sans-Serif; Color:#519cd4; text-align:center}"\
-        "</style>"\
-      "</head>"\
-      "<body>"\
-      /*
-        "<div><p>Led pin is now: "\
-          "<span id=\"led_state\">NE YASNO</span>"\
-        "</p></div>"\
-      */
-        "<div><p>"\
-          "<form method=\"post\" action=\"/req/post\">"\
-            "<p>Buttons on POST form</p>"\
-            "<button name=\"Led\" value=\"ON\">Turn On</button>"\
-            "<button name=\"Led\" value=\"OFF\">Turn Off</button>"\
-          "</form>"\
-        "</p></div>"\
-        "<div>"\
-          "<p>Measurements Control Panel</p>"\
-          "<p>"\
-            "<form method=\"post\" action=\"/req/post\">"\
-              "<button name=\"Msrmnt\" value=\"ON\">Start Measurements</button>"\
-              "<button name=\"Msrmnt\" value=\"OFF\">Stop Measurements</button>"\
-            "</form>"\
-          "</p>"\
-          "<p>"\
-            "<form method=\"get\" action=\"/req/get\">"\
-              "<button name=\"Download\" value=\"/SensorData.txt\">Download SensorData.txt button</button>"\
-            "</form>"\
-          "</p>"\
-        "</div>"\
-    /*
-    "<script>"\
-      "setInterval(function() {getLedState();}, 2000);"\
-      
-      "function getLedState() {"\
-        "var xhttp = new XMLHttpRequest();"\
-        "xhttp.onreadystatechange = function() {"\
-          "if (this.readyState == 4 && this.status == 200) {"\
-            "document.getElementById(\"led_state\").innerHTML = this.responseText;"\
-          "}"\
-        "};"\
-        "xhttp.open(\"GET\", \"/led_state\", true);"\
-        "xhttp.send();"\
-      "}"\
-    "</script>"\
-    */
-    /*
-    "<script>"\
-      "var ESPurl = \"/rest/events/subscribe\";"\
-      "if(typeof(EventSource) !== \"undefined\") {"\
-        "var source = new EventSource(ESPurl);"\
-        
-         * source.addEventListener('console', function(e) {\
-            "document.getElementById(\"console_text\").innerHTML += event.data + \"; \";"\
-           }, false);\
-        
-        "source.onmessage = function(event) {"\
-          "document.getElementById(\"console_text\").innerHTML += event.data + \"; \";"\
-        "};"\
-      "}"\
-      "else {"\
-        "document.write(\"Your browser does not support EventSource, buy a better one! \");"\
-        //"document.write(window.location.url);"
-      "}"\
-    "</script>"\
-    */
-    "</body>"\
-  "</html>";
+struct data {
+  time_t timeStomp;
+  int16_t ax, ay, az;
+};
+
+data dataStorage[MAX_DATA_SIZE];
 
 
 
 void SensorMeasurements() {
-  accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-  dataStorage[storageSize++] = {time(nullptr), ax, ay, az, gx, gy, gz};
-//  dataStorage.push_back(dataItem{ax, ay, az, gx, gy, gz}); //time(nullptr), 
-  Serial.println(ax);
-//    time_t now = time(nullptr);
-//    currentFile.println(ctime(&now));
-//    currentFile.print(" - ");
-//    
-//    // display tab-separated accel/gyro x/y/z values
-//    accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-//    
-//    currentFile.print("a/g:\t");
-//    currentFile.print(ax);  currentFile.print(", ");
-//    currentFile.print(ay);  currentFile.print(", ");
-//    currentFile.print(az);  currentFile.print(", ");
-//    currentFile.print(gx);  currentFile.print(", ");
-//    currentFile.print(gy);  currentFile.print(", ");
-//    currentFile.print(gz);  currentFile.println(";");
-//    currentFile.flush();
+  accelgyro.getAcceleration(&ax, &ay, &az);
+  time_t now = time(nullptr);
+  
+  dataStorage[iterations] = {now, ax, ay, az};
+
+  ++iterations;
+  
+  Serial.print(ax);  Serial.print(' ');
+  Serial.print(ay);  Serial.print(' ');
+  Serial.print(az);  Serial.println();
+  Serial.flush();
+//  Serial.println(accelgyro.getMotionStatus());
+//  Serial.println(accelgyro.getZeroMotionDetected());
 }
+
 
 
 void ParseConsole() {
   if (Serial.available() > 0) {
-    last_console_text = Serial.readStringUntil('\n');
-    Serial.print(F("console: \'"));
-    Serial.print(last_console_text);
-    Serial.println(F("\'"));
+    const String last_console_text = Serial.readStringUntil('\n');
     
-    if (last_console_text == "stop") {  // wi-fi
-      last_console_text.clear();
-      Serial.println(F("stopping wi-fi module"));
-      server.stop();
-      Serial.println(F("~~~"));
-      Serial.println();
-      return;
+  if (last_console_text == "stats") {
+      const unsigned long MeasureTime = Finish - Start;
+      Serial.print(F("WorkTime: "));
+      Serial.print((MeasureTime)/1000.0);
+      Serial.println(F("ms;"));
+      Serial.print(F("V = "));
+      Serial.print((MeasureTime/MAX_DATA_SIZE)/1000.0);
+//      Serial.print((MeasureTime/iterations)/1000.0);
+      Serial.println(F("kHz;"));
+      
+      FSInfo filesystem_info;
+      LittleFS.info(filesystem_info);
+      Serial.println(filesystem_info.usedBytes);
+      Serial.println(filesystem_info.totalBytes);
+      
+      Serial.print(F("SaveTime: "));
+      Serial.print(savetime/1000.0);
+      Serial.println(F("ms;"));
+      
+//      Serial.print(F("Size of time_t : "));
+//      Serial.println(sizeof(time_t));
+      //8
     }
-    else if (last_console_text == "stats") {
-      last_console_text.clear();
-      Serial.println(F("~Memory Stats~"));
-      Serial.print(F("FreeHeap: "));
-      Serial.println(ESP.getFreeHeap(), DEC);
-      Serial.print(F("HeapFragmentation: "));
-      Serial.println(ESP.getHeapFragmentation());
-      Serial.print(F("FlashChipSize: "));
-      Serial.println(ESP.getFlashChipSize(), DEC);
-      Serial.println();
-      return;
+    else if (last_console_text == "msrmnt") {
+      SensorLive = !SensorLive;
+      if (SensorLive) {
+        Serial.println(F("ax ay az"));
+        Start = micros();
+      }
+      else {
+        Finish = micros();
+      }
     }
     Serial.flush();
   }
 }
 
 
-void readyBlinking() {
-  digitalWrite(LED_BUILTIN, LOW);   delay(250);
-  digitalWrite(LED_BUILTIN, HIGH);  delay(250);
-  digitalWrite(LED_BUILTIN, LOW);   delay(250);
-  digitalWrite(LED_BUILTIN, HIGH);  delay(250);
-  digitalWrite(LED_BUILTIN, LOW);   delay(250);
-  digitalWrite(LED_BUILTIN, HIGH);  delay(250);
-}
-
-
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, led_state);
+//  system_update_cpu_freq(160);
   
-  Serial.begin(115200);
+  Serial.begin(1000000);
   delay(2500);
   
   Serial.println(F("Initializing GY-87..."));
   Serial.flush();
 
   Wire.begin();
+//  Wire.setClock(400000);
   accelgyro.initialize();
-  
-  accelgyro.setI2CMasterModeEnabled(false);
-  accelgyro.setI2CBypassEnabled(true) ;
-  accelgyro.setSleepEnabled(false);
-  
   Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
-
-  if(!bmp.begin()) {
-    /* There was a problem detecting the BMP085 ... check your connections */
-    Serial.println(F("Ooops, no BMP085 detected ... Check your wiring or I2C ADDR!"));
-    while(1);
-  }
-  else {
-    Serial.println(F("Yees!"));
-  }
   
-  if(!mag.begin()) {
-    /* There was a problem detecting the HMC5883 ... check your connections */
-    Serial.println(F("Ooops, no HMC5883 detected ... Check your wiring!"));
-    while(1);
-  }
-  else {
-    Serial.println(F("Yees!Yeeeeees!"));
-  }
+//  Serial.print(F("DLPFMode:"));
+//  Serial.println(accelgyro.getDLPFMode());
+  //0
+//  accelgyro.setDLPFMode(1);
 
-  if( accelgyro.testConnection() && bmp.begin() && mag.begin() ) {
-    readyBlinking();
-  }
+//  Serial.print(F("DHPFMode: "));
+//  Serial.println(accelgyro.getDHPFMode());
+  //0
+
+//  Serial.print(F("ScaleAccel: "));
+//  Serial.println(accelgyro.getFullScaleAccelRange());
+  //0
   
-  // Connect to WiFi network
-  Serial.println();
-  Serial.println();
-  Serial.print(F("Connecting to "));
-  Serial.println(ssid);
+  Serial.println(F("\nTest: "));
+//  accelgyro.CalibrateAccel(1);
+  accelgyro.PrintActiveOffsets();
   
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print('.');
-  }
-  Serial.println();
-  Serial.println(F("WiFi connected"));
 
-
+//  Serial.println(F("\nTest2: "));
+//  Serial.print(F("MotionDetectionThreshold: "));
+//  Serial.println(accelgyro.getMotionDetectionThreshold());
+  //0
+//  Serial.print(F("MotionDetectionDuration: "));
+//  Serial.println(accelgyro.getMotionDetectionDuration());
+  //0
+//  Serial.print(F("SleepEnabled: "));
+//  Serial.println(accelgyro.getSleepEnabled());  
+  //0
+//  Serial.print(F("WakeCycleEnabled: "));
+//  Serial.println(accelgyro.getWakeCycleEnabled());  
+  //0
+//  Serial.print(F("TempSensorEnabled: "));
+//  Serial.println(accelgyro.getTempSensorEnabled());
+  //1
+//  Serial.print(F("ClockSource: "));
+//  Serial.println(accelgyro.getClockSource());
+  //1
+//  Serial.print(F("WakeFrequency: "));
+//  Serial.println(accelgyro.getWakeFrequency());
+  
+//  Serial.println(F("\nTest3: "));
+//  Serial.print(F("DeviceID: "));
+//  Serial.println(accelgyro.getDeviceID(), HEX);
+  //0x34
+  
+  
   Serial.print(F("Trying to filesystem: "));
   if(LittleFS.begin()) {
     Serial.println(F("success!"));
     FSInfo filesystem_info;
-    Serial.println(LittleFS.info(filesystem_info));
+    LittleFS.info(filesystem_info);
     Serial.println(filesystem_info.totalBytes);
     Serial.println(filesystem_info.usedBytes);
     
@@ -262,154 +171,35 @@ void setup() {
     return;
   }
   Serial.println();
-  
-  
-  Serial.println(F("Syncing time"));
-  configTime(3 * 3600, 0, "pool.ntp.org", "time.nist.gov"); //ua.pool.ntp.org
-  while (!time(nullptr)) {
-    Serial.print('.');
-    delay(100);
-  }
-
-  
-  server.on("/", [](){
-    server.send(200, "text/html", preparedHtmlPage);
-  });
-  
-  /**/
-  server.on("/req/get", HTTP_GET, [](){
-    String msg = "Number of args received:";
-    msg += server.args();      // получить количество параметров
-    msg += "\n";               // переход на новую строку
-    
-    for (int i = 0; i < server.args(); i++) {
-      msg += "Arg #" + (String)i + " –> "; // добавить текущее значение счетчика
-      msg += server.argName(i) + ": ";      // получить имя параметра
-      msg += server.arg(i) + "\n";          // получить значение параметра
-    }
-    
-    Serial.print(msg);
-
-    if (server.hasArg("Download")) {
-      const String filename = server.arg("Download");
-      Serial.println(F("Downloading"));
-      
-      File download = LittleFS.open(filename, "r");
-      if (download) {
-        server.sendHeader("Content-Type", "text/text");
-        String str = "attachment; filename=";
-        str += filename;
-        server.sendHeader("Content-Disposition", str);
-        server.sendHeader("Connection", "close");
-        server.streamFile(download, "application/octet-stream");
-        download.close();
-      }
-      else {
-        server.send(501);
-      }
-    }
-    else if (server.arg("Led") == "OFF") {
-      Serial.println(F("Led go OFF but on GET"));
-      led_state = HIGH;
-      digitalWrite(LED_BUILTIN, led_state);
-    }
-    
-    Serial.flush();
-    Serial.println();
-    
-    server.send(204);
-  });
-
-  
-  server.on("/req/post", HTTP_POST, [](){
-    String msg = "Number of args received:";
-    msg += server.args();      // получить количество параметров
-    msg += "\n";               // переход на новую строку
-    
-    for (int i = 0; i < server.args(); i++) {
-      msg += "Arg #" + (String)i + " –> "; // добавить текущее значение счетчика
-      msg += server.argName(i) + ": ";      // получить имя параметра
-      msg += server.arg(i) + "\n";          // получить значение параметра
-    }
-    
-    Serial.print(msg);
-    
-    if (server.arg("Led") == "ON") {
-      Serial.println(F("Led go ON"));
-      led_state = LOW;
-      digitalWrite(LED_BUILTIN, led_state);
-    }
-    else if (server.arg("Led") == "OFF") {
-      Serial.println(F("Led go OFF"));
-      led_state = HIGH;
-      digitalWrite(LED_BUILTIN, led_state);
-    }
-    else if (server.arg("Msrmnt") == "ON") {
-      Serial.println(F("Msrmnt start"));
-//      SensorEmulatingLive = true;
-      SensorLive = true;
-    }
-    else if (server.arg("Msrmnt") == "OFF") {
-      Serial.println(F("Msrmnt stop"));
-//      SensorEmulatingLive = false;
-      SensorLive = false;
-      
-      File currentFile;
-      currentFile = LittleFS.open(F("/SensorData.txt"), "a");
-      
-      Serial.print(F("dataStorage size = "));
-      Serial.println(storageSize);
-      for (int i = 0; i<storageSize; i++) {
-        currentFile.println(ctime(&dataStorage[i].timeStomp));
-        currentFile.print(dataStorage[i].ax); currentFile.print(',');
-        currentFile.print(dataStorage[i].ay); currentFile.print(',');
-        currentFile.print(dataStorage[i].az); currentFile.print(',');
-        currentFile.print(dataStorage[i].gx); currentFile.print(',');
-        currentFile.print(dataStorage[i].gy); currentFile.print(',');
-        currentFile.print(dataStorage[i].gz); currentFile.println(';');
-      }
-      storageSize = 0;
-//      Serial.println(dataStorage.size());
-//      
-//      for (const auto& el:dataStorage) {
-////        currentFile.println(ctime(&el.timeStomp));
-//        currentFile.print(el.ax); currentFile.print(',');
-//        currentFile.print(el.ay); currentFile.print(',');
-//        currentFile.print(el.az); currentFile.print(',');
-//        currentFile.print(el.gx); currentFile.print(',');
-//        currentFile.print(el.gy); currentFile.print(',');
-//        currentFile.print(el.gz); currentFile.println(';');
-//      }
-//      dataStorage.clear();
-      
-      currentFile.close();
-    }
-    
-    Serial.flush();
-    Serial.println();
-    
-    server.send(204);
-  });
-  
-  
-  // Start the server
-  server.begin();
-  Serial.println(F("Server started"));
-  
-  // Print the IP address
-  Serial.print(F("Use this URL to connect: "));
-  Serial.print(F("http://"));
-  Serial.print(WiFi.localIP());
-  Serial.println('/');
-  Serial.println(F("~~~~~~~~"));
 }
 
 
 
 void loop() {
   ParseConsole();
-  server.handleClient();
   if (SensorLive) {
     SensorMeasurements();
+//    if(iterations==MAX_DATA_SIZE) {
+//  //    SensorLive = false;
+//      Finish = micros();
+//      
+//      s2 = micros();
+//      FSInfo filesystem_info;
+//      LittleFS.info(filesystem_info);
+//      if(filesystem_info.usedBytes < filesystem_info.totalBytes/3) {
+//        File currentFile = LittleFS.open(F("/SensorData.txt"), "a");
+//        for (int i = 0; i<iterations; i++) {
+//          currentFile.println(ctime(&dataStorage[i].timeStomp));
+//          currentFile.print(dataStorage[i].ax); currentFile.print(',');
+//          currentFile.print(dataStorage[i].ay); currentFile.print(',');
+//          currentFile.print(dataStorage[i].az); currentFile.println(';');
+//        }
+//        currentFile.close();
+//  //      Serial.println(F("keks"));
+//      }
+//      f2 = micros();
+//      savetime += f2-s2;
+//      iterations = 0;
+//    }
   }
 }
