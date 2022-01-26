7141e35903f19e5c4576021bbf74593c4c26314a
SexyPumpkin <62780669+SexyPumpkin@users.noreply.github.com>
2022-01-26 17:58:44 +0200

    Second version via upload

--- machining-health-device.ino
+++ machining-health-device.ino
@@ -8,11 +8,13 @@
 #include <Adafruit_HMC5883_U.h>
 #include "I2Cdev.h"
 #include "MPU6050.h"
+//#include <Vector.h>
 
 #include <time.h>
 #include <String>
 
 #define LED_BUILTIN 2
+#define MAX_DATASIZE 1500
 
 
 const char* ssid = "yourNetwork";
@@ -21,10 +23,18 @@ const char* password = "secretPassword";
 String last_console_text = "";
 bool led_state = true;  //set off by default
 
-bool SensorEmulatingLive = false;
 bool SensorLive = false;
-bool SensorToConsole = false;
 
+
+struct dataItem {
+  time_t timeStomp;
+  int16_t ax, ay, az;
+  int16_t gx, gy, gz;
+};
+
+//Vector<dataItem> dataStorage;
+dataItem dataStorage[MAX_DATASIZE];
+size_t storageSize = 0;
 
 ESP8266WebServer server(80);
 
@@ -113,183 +123,26 @@ const String preparedHtmlPage =
 
 
 
-String get_led_state_in_string() {
-  if (led_state) {
-    return "NE GORIT";
-  }
-  return "GORIT";
-}
-
-
-
-void FileSytemTest(const String& filename = "/f.txt") {
-  File f = LittleFS.open(filename, "a+");
-  if (f) {
-    Serial.println("Filedata: ");
-    while (f.available()) {
-      Serial.print(f.readStringUntil('\n'));
-    }
-    Serial.println();
-    Serial.flush();
-    
-    //f.println("+1");
-    f.flush();
-    f.close();
-  }
-  else {
-    Serial.println(F("file openning error"));
-  }
-}
-
-
-
-void EmulateSensor() {
-  //Serial.println("SensorEmulator");
-  const String filename = "/DummyData.txt";
-  File f = LittleFS.open(filename, "a");
-  if (f) {
-    const int tmp = rand() % 100;
-    time_t now = time(nullptr);
-    
-    f.print(ctime(&now));
-    f.print(" - ");
-    f.print(tmp);
-    f.println(";");
-    f.flush();
-    
-    f.close();
-  }
-  else {
-    Serial.println(F("file openning error"));
-  }
-}
-
-
-
-void SensorMeasurementsToConsole() {
-  accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-  Serial.print(ax); Serial.print(' ');
-  Serial.print(ay); Serial.print(' ');
-  Serial.print(az); Serial.println(' ');
-  Serial.flush();
-  return;
-  
-  File f = LittleFS.open("/SensorData.txt", "a");
-  if (f) {
-    time_t now = time(nullptr);
-    f.print(ctime(&now));
-    f.print(F(" - "));
-    
-    // display tab-separated accel x/y/z values
-    accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-    Serial.print(ax); Serial.print(' ');
-    Serial.print(ay); Serial.print(' ');
-    Serial.println(az);
-    
-    f.print("a: ");
-    f.print(ax); f.print(", ");
-    f.print(ay); f.print(", ");
-    f.print(az); f.println(';');
-    f.flush();
-      
-    f.close();
-  }
-  else {
-    Serial.println(F("file openning error"));
-  }
-}
-
-
 void SensorMeasurements() {
-  const String filename = "/SensorData.txt";
-  File f = LittleFS.open(filename, "a");
-  if (f) {
-    time_t now = time(nullptr);
-    f.println(ctime(&now));
-    f.print(" - ");
-    
-    sensors_event_t event;
-    bmp.getEvent(&event);
-  
-    if (event.pressure) {
-      Serial.print("Pressure:    ");
-      Serial.print(event.pressure);
-      Serial.println(" hPa");
-      
-      float temperature;
-      bmp.getTemperature(&temperature);
-      Serial.print("Temperature: ");
-      Serial.print(temperature);
-      Serial.println(" C");
-      
-      float seaLevelPressure = SENSORS_PRESSURE_SEALEVELHPA;
-      Serial.print("Altitude:    "); 
-      Serial.print(bmp.pressureToAltitude(seaLevelPressure, event.pressure)); 
-      Serial.println(" m");
-
-      f.print("P: ");   f.print(event.pressure);    f.println(" hPa");
-      f.print("T: ");   f.print(temperature);       f.println(" C");
-      f.print("A: ");   f.print(bmp.pressureToAltitude(seaLevelPressure, event.pressure));  f.println(" m");
-    }
-    else {
-      Serial.println("Sensor1 error");
-    }
-    
-    
-    // display tab-separated accel/gyro x/y/z values
-    accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-    Serial.print("a/g:\t");
-    Serial.print(ax); Serial.print(", ");
-    Serial.print(ay); Serial.print(", ");
-    Serial.print(az); Serial.print(", ");
-    Serial.print(gx); Serial.print(", ");
-    Serial.print(gy); Serial.print(", ");
-    Serial.println(gz);
-    
-    mag.getEvent(&event);
-    /* Display the results (magnetic vector values are in micro-Tesla (uT)) */
-    Serial.print("X: "); Serial.print(event.magnetic.x); Serial.print(", ");
-    Serial.print("Y: "); Serial.print(event.magnetic.y); Serial.print(", ");
-    Serial.print("Z: "); Serial.print(event.magnetic.z); Serial.print(" ");Serial.println("uT");
-    
-    f.print("a/g:\t");
-    f.print(ax); f.print(", ");
-    f.print(ay); f.print(", ");
-    f.print(az); f.print(", ");
-    f.print(gx); f.print(", ");
-    f.print(gy); f.print(", ");
-    f.println(gz);
-
-//    f.print("X: "); f.print(event.magnetic.x); f.print(", ");
-//    f.print("Y: "); f.print(event.magnetic.y); f.print(", ");
-//    f.print("Z: "); f.print(event.magnetic.z); f.print("  "); f.println("uT");
-
-    
-    float heading = atan2(event.magnetic.y, event.magnetic.x);
-    
-    float declinationAngle = +5.22;
-    heading += declinationAngle;
-    
-    if (heading < 0)
-      heading += 2*PI;
-    
-    if (heading > 2*PI)
-      heading -= 2*PI;
-    
-    float headingDegrees = heading * 180/M_PI;
-    Serial.print("Heading (degrees): "); Serial.println(headingDegrees);
-    Serial.println();
-    
-    f.print("Heading (degrees): "); f.println(headingDegrees);
-    f.println(";");
-    f.flush();
-      
-    f.close();
-    delay(5); 
-  }
-  else {
-    Serial.println(F("file openning error"));
-  }
+  accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+  dataStorage[storageSize++] = {time(nullptr), ax, ay, az, gx, gy, gz};
+//  dataStorage.push_back(dataItem{ax, ay, az, gx, gy, gz}); //time(nullptr), 
+  Serial.println(ax);
+//    time_t now = time(nullptr);
+//    currentFile.println(ctime(&now));
+//    currentFile.print(" - ");
+//    
+//    // display tab-separated accel/gyro x/y/z values
+//    accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+//    
+//    currentFile.print("a/g:\t");
+//    currentFile.print(ax);  currentFile.print(", ");
+//    currentFile.print(ay);  currentFile.print(", ");
+//    currentFile.print(az);  currentFile.print(", ");
+//    currentFile.print(gx);  currentFile.print(", ");
+//    currentFile.print(gy);  currentFile.print(", ");
+//    currentFile.print(gz);  currentFile.println(";");
+//    currentFile.flush();
 }
 
 
@@ -320,39 +173,7 @@ void ParseConsole() {
       Serial.println();
       return;
     }
-    else if (last_console_text == "fstest") {
-      last_console_text.clear();
-      Serial.println(F("File system check"));
-      
-      time_t now = time(nullptr);
-      Serial.println(ctime(&now));
-      
-      //FileSytemTest();
-      FileSytemTest(F("/DummyData.txt"));
-      Serial.println();
-    }
-    else if (last_console_text == "emul") {
-      SensorEmulatingLive = !SensorEmulatingLive;
-      Serial.print(F("Emulating is "));
-      Serial.print(SensorEmulatingLive);
-      Serial.println(F(" now"));
-    }
-    else if (last_console_text == "measure") {
-      SensorLive = !SensorLive;
-      Serial.print(F("Measuring is "));
-      Serial.print(SensorEmulatingLive);
-      Serial.println(F(" now"));
-    }
-    else if (last_console_text == "measuretoconsole") {
-      SensorToConsole = !SensorToConsole;
-      led_state = !led_state;
-      digitalWrite(LED_BUILTIN, led_state);
-      
-      Serial.println(F("ax ay az"));
-//      Serial.print(F("Measuring is "));
-//      Serial.print(SensorToConsole);
-//      Serial.println(F(" now"));
-    }
+    Serial.flush();
   }
 }
 
@@ -407,36 +228,12 @@ void setup() {
   if( accelgyro.testConnection() && bmp.begin() && mag.begin() ) {
     readyBlinking();
   }
-
-  //ya sohranil
-  Serial.print(accelgyro.getXAccelOffset());  Serial.print("\t"); // -3572
-  Serial.print(accelgyro.getYAccelOffset());  Serial.print("\t"); // -1871
-  Serial.print(accelgyro.getZAccelOffset());  Serial.print("\t"); // 1486
-  Serial.print(accelgyro.getXGyroOffset());   Serial.print("\t"); // 0
-  Serial.print(accelgyro.getYGyroOffset());   Serial.print("\t"); // 0
-  Serial.print(accelgyro.getZGyroOffset());   Serial.print("\t"); // 0
-  Serial.print("\n");
-//  accelgyro.setXGyroOffset(220);
-//  accelgyro.setYGyroOffset(76);
-//  accelgyro.setZGyroOffset(-85);
-
-//  accelgyro.setXAccelOffset(0);
-//  accelgyro.setYAccelOffset(0);
-//  accelgyro.setZAccelOffset(0);
-  
-//  delay(2500);
-
   
   // Connect to WiFi network
   Serial.println();
   Serial.println();
   Serial.print(F("Connecting to "));
   Serial.println(ssid);
-
-  
-//  if (WiFi.softAP("ESPsoftAP_test", "tester123")) {
-//    readyBlinking();
-//  }
   
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
@@ -454,14 +251,6 @@ void setup() {
     Serial.println(LittleFS.info(filesystem_info));
     Serial.println(filesystem_info.totalBytes);
     Serial.println(filesystem_info.usedBytes);
-
-    const String filename1 = "/DummyData.txt";
-    if(LittleFS.exists(filename1)) {
-      LittleFS.remove(filename1);
-    }
-    else {
-      Serial.println(F("Wow, really?"));
-    }
     
     const String filename2 = "/SensorData.txt";
     if(LittleFS.exists(filename2)) {
@@ -486,14 +275,6 @@ void setup() {
   server.on("/", [](){
     server.send(200, "text/html", preparedHtmlPage);
   });
-  
-  
-  server.on("/led-state-ping", [](){
-    const String str = "data: "+ get_led_state_in_string() +"\n\n";
-    server.sendHeader("Cache-Control", "no-cache");
-    server.send(200, "text/event-stream", str);
-  });
-  
   
   /**/
   server.on("/req/get", HTTP_GET, [](){
@@ -566,12 +347,42 @@ void setup() {
     else if (server.arg("Msrmnt") == "ON") {
       Serial.println(F("Msrmnt start"));
 //      SensorEmulatingLive = true;
-        SensorLive = true;
+      SensorLive = true;
     }
     else if (server.arg("Msrmnt") == "OFF") {
       Serial.println(F("Msrmnt stop"));
 //      SensorEmulatingLive = false;
-        SensorLive = false;
+      SensorLive = false;
+      
+      File currentFile;
+      currentFile = LittleFS.open(F("/SensorData.txt"), "a");
+      
+      Serial.print(F("dataStorage size = "));
+      Serial.println(storageSize);
+      for (int i = 0; i<storageSize; i++) {
+        currentFile.println(ctime(&dataStorage[i].timeStomp));
+        currentFile.print(dataStorage[i].ax); currentFile.print(',');
+        currentFile.print(dataStorage[i].ay); currentFile.print(',');
+        currentFile.print(dataStorage[i].az); currentFile.print(',');
+        currentFile.print(dataStorage[i].gx); currentFile.print(',');
+        currentFile.print(dataStorage[i].gy); currentFile.print(',');
+        currentFile.print(dataStorage[i].gz); currentFile.println(';');
+      }
+      storageSize = 0;
+//      Serial.println(dataStorage.size());
+//      
+//      for (const auto& el:dataStorage) {
+////        currentFile.println(ctime(&el.timeStomp));
+//        currentFile.print(el.ax); currentFile.print(',');
+//        currentFile.print(el.ay); currentFile.print(',');
+//        currentFile.print(el.az); currentFile.print(',');
+//        currentFile.print(el.gx); currentFile.print(',');
+//        currentFile.print(el.gy); currentFile.print(',');
+//        currentFile.print(el.gz); currentFile.println(';');
+//      }
+//      dataStorage.clear();
+      
+      currentFile.close();
     }
     
     Serial.flush();
@@ -579,7 +390,7 @@ void setup() {
     
     server.send(204);
   });
-
+  
   
   // Start the server
   server.begin();
@@ -598,13 +409,7 @@ void setup() {
 void loop() {
   ParseConsole();
   server.handleClient();
-  if (SensorEmulatingLive) {
-    EmulateSensor();
-  }
   if (SensorLive) {
     SensorMeasurements();
-  }
-  if (SensorToConsole) {
-    SensorMeasurementsToConsole();
   }
 }
